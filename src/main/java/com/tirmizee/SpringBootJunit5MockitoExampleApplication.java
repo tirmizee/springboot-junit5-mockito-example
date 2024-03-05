cc4ed9816501a794eabd7e8c7d9ead60f5aaa3a8
pratya <pratya@cardx.co.th>
2024-03-05 12:33:29 +0700

    init

---
+++ src/main/java/com/tirmizee/SpringBootJunit5MockitoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJunit5MockitoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJunit5MockitoExampleApplication.class, args);
+	}
+
+}
